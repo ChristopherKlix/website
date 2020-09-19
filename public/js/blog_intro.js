90ba0ec64f80fcadd06ad654295d3de214f12a13
ChristopherKlix <70777673+ChristopherKlix@users.noreply.github.com>
2020-09-19 13:33:45 +0200

    update scss

--- public/js/blog_intro.js
+++ public/js/blog_intro.js
@@ -16,3 +16,17 @@ function blogIntro() {
     }, 3000)
 
 };
+
+function redirect_to_podcast() {
+    $('#blog-intro-overlay').css('transition-duration', '1s');
+    $('#blog-intro-overlay').css('left', '0');
+
+    setTimeout(() => {
+        $('#blog-intro-p').css('transition-duration', '1s');
+        $('#blog-intro-p').css('opacity', '1');
+    }, 1000);
+
+    setTimeout(() => {
+        window.location.href = "https://generation-space.simplecast.com/";
+    }, 3000)
+}
